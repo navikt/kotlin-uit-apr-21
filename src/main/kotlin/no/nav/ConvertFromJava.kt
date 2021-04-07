0de8955a0a30511bbcc96adfb5f17f7d42a8288a
J-K. Solbakken <jksolbakken@gmail.com>
2021-04-07 21:53:41 +0200

    convert java to kotlin

---
+++ src/main/kotlin/no/nav/ConvertFromJava.kt
@@ -0,0 +1,10 @@
+package no.nav
+
+/*
+Paste this class below in IntelliJ to convert to Kotlin
+public class Stuff {
+   public void doThings() {
+      System.out.println("Stuff doing things");
+   }
+}
+ */
