3935ba77f35e6697615c61a7fe74df4d81a82f3b
J-K. Solbakken <jksolbakken@gmail.com>
2021-04-13 08:47:33 +0200

    function currying

--- src/main/kotlin/no/nav/Functions.kt
+++ src/main/kotlin/no/nav/Functions.kt
@@ -6,6 +6,7 @@ fun main() {
    println(doStuffWithNumbers(2, 3) { x, y -> x * y }) // lambda
    println(doStuffWithNumbers(2, 3, ::add))            // function reference
    println(adder()(2, 3))
+   println(curriedAdder(2)(3)(4))
    println(multiplier(2, 3))
    withDefaultArg(first = "one")
    withDefaultArg(first = "one", second = "overridden")
@@ -18,6 +19,8 @@ fun add(x: Int, y: Int): Int = x + y
 fun doStuffWithNumbers(x: Int, y: Int, operation: (Int, Int) -> Int)  = operation(x, y)
 
 fun adder() = { x: Int, y: Int -> x + y }
+
+fun curriedAdder(x: Int) = { y: Int -> { z: Int -> x + y + z } }
 
 val multiplier = { x: Int, y: Int -> x * y }
 
